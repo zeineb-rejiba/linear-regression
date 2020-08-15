1d24ec5c2d266a29807f340ac973e4db876e846a
zeineb-rejiba <zeineb.rejiba@gmail.com>
2020-08-15 12:39:49 +0200

    disable unnecessary tensorflow debugging

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 
 
--- plot_utils.py
+++ plot_utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
 from model import Model
